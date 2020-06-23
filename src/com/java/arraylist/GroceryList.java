5287bb64110a487f492e1bc4338725fee9478d73
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-06-23 20:41:07 +0600

    Implement Basic Grocery CRUD

---
+++ src/com/java/arraylist/GroceryList.java
@@ -0,0 +1,32 @@
+package com.java.arraylist;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    //add item
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+    }
+
+    // print all the items from grocerylist
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocecy list");
+        for(int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i + 1) + ". " + groceryList.get(i));
+        }
+    }
+
+    // modify grocery item
+    public void modifyGroceryItem(int index, String item) {
+        groceryList.set(index, item);
+        System.out.println("Grocery item " + (index+1) + " has been updated");
+    }
+
+    // remove grocery item
+    public void removeGroceryItem(int index) {
+        String item = groceryList.get(index);
+        groceryList.remove(item);
+    }
+}
---
+++ src/com/java/arraylist/Main.java
@@ -0,0 +1,9 @@
+package com.java.arraylist;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
